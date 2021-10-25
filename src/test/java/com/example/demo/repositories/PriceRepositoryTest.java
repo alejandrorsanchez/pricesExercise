1224bb2905642d49b78915046e1d5e0c6460e7b1
aleandrorsanchez <aleeexr7@gmail.com>
2021-10-25 14:15:25 +0200

    new test for PriceRepositoryTest

--- src/test/java/com/example/demo/repositories/PriceRepositoryTest.java
+++ src/test/java/com/example/demo/repositories/PriceRepositoryTest.java
@@ -11,6 +11,7 @@ import java.time.Month;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 public class PriceRepositoryTest {
@@ -19,17 +20,49 @@ public class PriceRepositoryTest {
     private PriceRepository priceRepository;
 
     @Test
-    void testFindByStartDateLessThanAndEndDateGreaterThanAndProductIdAndBrandIdOrderByPriorityDesc(){
-        LocalDateTime date = LocalDateTime.of(2020, Month.JUNE, 14, 10, 00, 00);
-        BrandEntity brandEntity = new BrandEntity(1, "ZARA");
-        PriceEntity priceEntity = this.priceRepository.findFirstByStartDateLessThanAndEndDateGreaterThanAndProductIdAndBrandOrderByPriorityDesc(date, date,35455,brandEntity);
-        assertEquals(35.5, priceEntity.getPrice());
-    }
-
-    @Test
     void testVerification(){
         Optional<PriceEntity> priceEntity = this.priceRepository.findById(0);
         System.out.println(priceEntity);
         assertEquals(35.5, priceEntity.orElseThrow().getPrice());
+    }
+
+    @Test
+    void testFindByStartDateLessThanAndEndDateGreaterThanAndProductIdAndBrandIdOrderByPriorityDesc1(){
+        LocalDateTime date = LocalDateTime.of(2020, Month.JUNE, 14, 10, 00, 00);
+        BrandEntity brandEntity = new BrandEntity(1, "ZARA");
+        PriceEntity priceEntity = this.priceRepository.findFirstByStartDateLessThanAndEndDateGreaterThanAndProductIdAndBrandOrderByPriorityDesc(date, date,35455,brandEntity);
+        assertEquals(35.50, priceEntity.getPrice());
+    }
+
+    @Test
+    void testFindByStartDateLessThanAndEndDateGreaterThanAndProductIdAndBrandIdOrderByPriorityDesc2(){
+        LocalDateTime date = LocalDateTime.of(2020, Month.JUNE, 14, 16, 00, 00);
+        BrandEntity brandEntity = new BrandEntity(1, "ZARA");
+        PriceEntity priceEntity = this.priceRepository.findFirstByStartDateLessThanAndEndDateGreaterThanAndProductIdAndBrandOrderByPriorityDesc(date, date,35455,brandEntity);
+        assertEquals(25.45, priceEntity.getPrice());
+    }
+
+    @Test
+    void testFindByStartDateLessThanAndEndDateGreaterThanAndProductIdAndBrandIdOrderByPriorityDesc3(){
+        LocalDateTime date = LocalDateTime.of(2020, Month.JUNE, 14, 21, 00, 00);
+        BrandEntity brandEntity = new BrandEntity(1, "ZARA");
+        PriceEntity priceEntity = this.priceRepository.findFirstByStartDateLessThanAndEndDateGreaterThanAndProductIdAndBrandOrderByPriorityDesc(date, date,35455,brandEntity);
+        assertEquals(35.50, priceEntity.getPrice());
+    }
+
+    @Test
+    void testFindByStartDateLessThanAndEndDateGreaterThanAndProductIdAndBrandIdOrderByPriorityDesc4(){
+        LocalDateTime date = LocalDateTime.of(2020, Month.JUNE, 15, 10, 00, 00);
+        BrandEntity brandEntity = new BrandEntity(1, "ZARA");
+        PriceEntity priceEntity = this.priceRepository.findFirstByStartDateLessThanAndEndDateGreaterThanAndProductIdAndBrandOrderByPriorityDesc(date, date,35455,brandEntity);
+        assertEquals(30.50, priceEntity.getPrice());
+    }
+
+    @Test
+    void testFindByStartDateLessThanAndEndDateGreaterThanAndProductIdAndBrandIdOrderByPriorityDesc5(){
+        LocalDateTime date = LocalDateTime.of(2020, Month.JUNE, 16, 21, 00, 00);
+        BrandEntity brandEntity = new BrandEntity(1, "ZARA");
+        PriceEntity priceEntity = this.priceRepository.findFirstByStartDateLessThanAndEndDateGreaterThanAndProductIdAndBrandOrderByPriorityDesc(date, date,35455,brandEntity);
+        assertEquals(38.95, priceEntity.getPrice());
     }
 }
